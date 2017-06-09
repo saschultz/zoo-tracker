eeea1e1352aa36a920ff8197ee085e1c1dcc0ae6
Sara Schultz <saschultz95@gmail.com>
2017-06-09 10:22:15 -0700

    import animal-list component in app.module and link selector tag in root component

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -1,2 +1,15 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Animal } from './animal.model';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+  <ul>
+    <li></li>
+  </ul>
+  `
+})
+
+export class AnimalListComponent {
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -10,6 +10,7 @@ import { Animal } from './animal.model';
  </div>
  <div class="container">
   <h2>Animal List</h2>
+  <animal-list></animal-list>
  </div>
   `
 })
