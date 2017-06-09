3c282f6d35a5106f5a30e3bf7bf94955106bbc83
Sara Schultz <saschultz95@gmail.com>
2017-06-09 13:59:02 -0700

    add functionality to sort animal list by age

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -4,8 +4,14 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'animal-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+    <option value="allAnimals"  selected="selected">All Animals</option>
+    <option value="babyAnimals">Baby Animals</option>
+    <option value="adultAnimals">Adult Animals</option>
+  </select>
+  
   <ul>
-    <li *ngFor="let currentAnimal of childAnimalList"><b>{{currentAnimal.species}}</b><br>located in the  {{currentAnimal.location}}<br><b>Animal Profile</b><br><b>{{currentAnimal.name}}</b> <em>{{currentAnimal.age}} years old, {{currentAnimal.sex}}</em><br>{{currentAnimal.diet}}<br> likes: {{currentAnimal.likes}}<br>dislikes: {{currentAnimal.dislikes}}<br>Number of caretakers: {{currentAnimal.caretakers}}<br><button (click)="editButtonClicked(currentAnimal)">edit</button><br><br></li>
+    <li *ngFor="let currentAnimal of childAnimalList | age:filterByAge"><b>{{currentAnimal.species}}</b><br>located in the  {{currentAnimal.location}}<br><b>Animal Profile</b><br><b>{{currentAnimal.name}}</b> <em>{{currentAnimal.age}} years old, {{currentAnimal.sex}}</em><br>{{currentAnimal.diet}}<br> likes: {{currentAnimal.likes}}<br>dislikes: {{currentAnimal.dislikes}}<br>Number of caretakers: {{currentAnimal.caretakers}}<br><button (click)="editButtonClicked(currentAnimal)">edit</button><br><br></li>
   </ul>
   `
 })
@@ -13,6 +19,11 @@ import { Animal } from './animal.model';
 export class AnimalListComponent {
   @Input() childAnimalList: Animal[];
   @Output() clickSender = new EventEmitter();
+  filterByAge: string = "allAnimals";
+
+  onChange(optionFromMenu) {
+    this.filterByAge = optionFromMenu;
+  }
 
   editButtonClicked(animalToEdit: Animal) {
     this.clickSender.emit(animalToEdit);
