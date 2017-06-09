90a527f62c386b181a76324fe741c4c8eeec4f2a
Sara Schultz <saschultz95@gmail.com>
2017-06-09 11:06:40 -0700

    add edit form and input/output preparation in edit-animal component

--- app/edit-animal.component.ts
+++ app/edit-animal.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Animal } from './animal.model';
 
 @Component({
   selector: 'edit-animal',
   template: `
+  <div *ngIf="childSelectedAnimal">
+    <h3>Edit {{childSelectedAnimal.name}}</h3>
+    <label>Name</label><br>
+    <input [(ngModel)]="childSelectedAnimal.name">
+    <label>Age</label><br>
+    <input [(ngModel)]="childSelectedAnimal.age">
+    <label>Caretakers</label><br>
+    <input [(ngModel)]="childSelectedAnimal.caretakers">
+    <button (click)="doneButtonClicked()">Done</button>
+  </div>
   `
 })
 
 export class EditAnimalComponent {
   @Input() childSelectedAnimal: Animal;
+  @Output() doneButtonClickedSender = new EventEmitter();
+
+  doneButtonClicked() {
+    this.doneButtonClickedSender.emit();
+  }
 }
