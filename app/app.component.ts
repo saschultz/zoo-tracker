9c917c65cdc5e345732296eede1c1f24f763dd48
Sara Schultz <saschultz95@gmail.com>
2017-06-09 10:05:34 -0700

    correct author in bower.json and add basic html structure in root component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-   <h1>zoo-tracker</h1>
+  <div class="jumbotron">
+   <h1>Zoo Tracker</h1>
+ </div>
+ <div class="container">
+  <h2>Animal List</h2>
  </div>
   `
 })
@@ -12,4 +15,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
 }
-
