4b44b9f1a53fb48ec0a5bfe31e9b82af6e3ffc41
Sara Schultz <saschultz95@gmail.com>
2017-06-09 16:30:04 -0700

    add more hard coded animal data

--- app/app.component.ts
+++ app/app.component.ts
@@ -29,7 +29,11 @@ export class AppComponent {
   masterAnimalList: Animal[] = [
     new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5, "Female", "Cool shade", "Loud noises"),
     new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Female", "Delicate roots and leaves", "Loud noises"),
-    new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the shade", "Toys")
+    new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the shade", "Toys"),
+    new Animal("Red Panda", "Sara", 1, "Herbivore", "Tropical Rain Forest Building", 4, "Female", "Eating bamboo", "People"),
+    new Animal("Zebra", "Kevin", 12, "Herbivore", "Grassland Building", 3, "Male", "People", "Eating bamboo"),
+    new Animal("Penguin", "Lionel", 2, "Carnivore", "Arctic Building", 1, "Male", "Sliding around", "Falling"),
+    new Animal("Penguin", "Lilly", 3, "Carnivore", "Arctic Building", 1, "Female", "Chilling", "Falling")
   ];
   selectedAnimal = null;
 
