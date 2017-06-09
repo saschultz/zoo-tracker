2891a0d6d153c31db0c0fc504fa82c75bb4e11e6
Sara Schultz <saschultz95@gmail.com>
2017-06-09 10:15:12 -0700

    add hardcoded animal data in root component

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,5 +15,9 @@ import { Animal } from './animal.model';
 })
 
 export class AppComponent {
-
+  masterAnimalList: Animal[] = [
+    new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5, "Female", "Cool shade", "Loud noises"),
+    new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Female", "Delicate roots and leaves", "Loud noises"),
+    new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the shade", "Toys")
+  ];
 }
