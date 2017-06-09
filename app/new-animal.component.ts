28227896d24f55279141e5a2451397d488bf3367
Sara Schultz <saschultz95@gmail.com>
2017-06-09 12:42:19 -0700

    add form and output preparation for adding new animal

--- app/new-animal.component.ts
+++ app/new-animal.component.ts
@@ -5,9 +5,36 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'new-animal',
   template: `
+  <h2>Add Animal</h2>
+  <div>
+    <label>Animal Species:</label>
+    <input #newSpecies>
+    <label>Animal Name:</label>
+    <input #newName>
+    <label>Animal Age:</label>
+    <input #newAge>
+    <label>Animal Diet:</label>
+    <input #newDiet>
+    <label>Animal Location:</label>
+    <input #newLocation>
+    <label>Animal Caretakers:</label>
+    <input #newCaretakers>
+    <label>Animal Sex:</label>
+    <input #newSex>
+    <label>Animal Likes:</label>
+    <input #newLikes>
+    <label>Animal Dislikes:</label>
+    <input #newDislikes>
+    <button (click)="submitForm(newSpecies.value, newName.value, newAge.value, newDiet.value, newLocation.value, newCaretakers.value, newSex.value, newLikes.value, newDislikes.value; newSpecies.value=''; newName.value=''; newAge.value=''; newDiet.value=''; newLocation.value=''; newCaretakers.value=''; newSex.value=''; newLikes.value=''; newDislikes.value='';)">Add</button>
+  </div>
   `
 })
 
 export class NewAnimalComponent {
-  
+  @Output() newAnimalSender = new EventEmitter();
+
+  submitForm(species: string, name: string, age: number, diet: string, location: string, caretakers: number, sex: string, likes: string, dislikes: string) {
+    var newAnimalToAdd: Animal = new Animal(species, name, age, diet, location, caretakers, sex, likes, dislikes);
+    this.newAnimalSender.emit(newAnimalToAdd);
+  }
 }
